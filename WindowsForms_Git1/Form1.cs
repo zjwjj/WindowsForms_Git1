de9b29159f0a1fb1b4ae125da871c02c673dcd19
jj w <zjwjj@outlook.com>
2019-05-12 19:04:03 +0800

    添加按钮

--- WindowsForms_Git1/Form1.cs
+++ WindowsForms_Git1/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsForms_Git1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello Git");
+        }
     }
 }
