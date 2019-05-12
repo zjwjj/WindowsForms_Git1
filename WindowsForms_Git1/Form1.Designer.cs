e16af637c54efef9edd7c4ed836a52affbe78666
jj w <zjwjj@outlook.com>
2019-05-12 19:40:33 +0800

    添加按钮4

--- WindowsForms_Git1/Form1.Designer.cs
+++ WindowsForms_Git1/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -64,18 +65,28 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(500, 396);
+            this.button3.Location = new System.Drawing.Point(285, 396);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(288, 42);
             this.button3.TabIndex = 3;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(644, 396);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(132, 42);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -93,6 +104,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
